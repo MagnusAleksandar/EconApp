b7a130451eb2c00872ebb269fd22df6a66511c55
harri <harri@DESKTOP-L45Q02L>
2022-12-12 10:54:13 -0500

    fañtan cosillas

--- EconApp/src/main/java/Modelo/Casa.java
+++ EconApp/src/main/java/Modelo/Casa.java
@@ -1,6 +1,5 @@
 package Modelo;
 import Vista.InOut;
-import java.io.BufferedWriter;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -10,158 +9,98 @@ import java.util.ArrayList;
 
 public class Casa {
     static InOut v=new InOut();
-    double ahorro,gt;
-    Contribuyentes co[];
-    Gasto g[];
-    Persona p[];
+    double ahorro;
     double ToAporte,ToGastos;
     private ArrayList<Persona> personas;
-    private ArrayList<Contribuyente> cont;
+    private ArrayList<Contribuyentes> cont;
     private ArrayList<Gasto> gast;
- 
     public File gastos, ahorros;
- 
-    void pedirIntegrantes(){
-       int in=0,c = 0;
-       String nom;
-       char a;
-       boolean b;
-       in=v.solicitarEntero("ingrese cantidad de integrantes: ");
-         for(int i=0;i<in;i++){
-             nom=v.solicitarString("Nombre:");
-             a=v.pedirChar("Es aportante?(s/n)");
-             b=validarChar(a);
-             do{
-                  if(!b){
-                 v.mostrarRes("Ingrese una opcion valida");
-             }else{
-                 if(a=='s'||a=='S'){
-                     pedirAportante(nom,c);
-                 }
-             }
-             }while(!b);
-                     }
-   }
-    void pediAportante(String nom,int c){
-    double ap;
-    ap=v.solicitarDouble("ingrese valor del aporte");
-     co[c]=Contribuyente(nom,ap);
-             ToAporte+=ap;
-            
 
-}
-
-
-    void mostar(){
-       String m="";
-      for(int i=0;i<co.length;i++){
-            m +="Nombre: " + p.getNom()+" aporte:"+co.getContribucion();
-        }
-       v.mostrarRes(m);
-   }
-    void PedirGastos(){
-       String des;
-       double gas;
-       boolean b;
-       int i=0;
-       char a = 0;
-       while(a!='N'||a!='n'){
-           des=v.solicitarString("Ingrese descripcion del gasto:");
-           gas=v.solicitarDouble("Ingrese valor del gasto:");
-           
-           do{
-           a=v.pedirChar("Quiere Agregar otro gasto:");
-           b=validarChar(a);
-           if(!b)
-             v.mostrarRes("ingrese una opcion valida");
-       }while(!b);
-           g[i]=Gasto(des,gas);
-           i++;
-           ToGastos+=gas;
-       }
-       
-   }
-    boolean validarChar(char a){
-       if(a=='S' || a=='s' || a=='N' || 'n'==a){
-           return true;
-       }else{
-           return false;
-       }
-   }
-
-
-    void ListIntegrantes(){
-   
-    ArrayList<Persona> personas = new ArrayList<Persona>();
-     int in=0;
-       String nom = null;
-       char a;
-       boolean b;
-       in=v.solicitarEntero("ingrese cantidad de integrantes: ");
-         for(int i=0;i<in;i++){
-             nom=v.solicitarString("Nombre:");
-             a=v.pedirChar("Es aportante?(s/n)");
-             b=validarChar(a);
-             do{
-                  if(!b){
-                 v.mostrarRes("Ingrese una opcion valida");
-             }else{
-                 if(a=='s'||a=='S'){
-                     ListAportante(nom);
-                 }
-             }
-             }while(!b);
-                     }
-        personas.add(new Persona(nom));
-}
-
-    void ListAportante(String nom){
-    ArrayList<Contribuyente> cont=new ArrayList<Contribuyente>();
-    double ap;
-    ap=v.solicitarDouble("ingrese valor del aporte");
-             ToAporte+=ap;
-         cont.add(new Contribuyentes(nom,ap));   
-
+    public void pedirIntegrantes(int ban){
+        int in, a, prc;
+        String nom;
+        boolean b=false;
+        double slr, cnt=0;
+        in=v.solicitarEntero("Ingrese cantidad de integrantes: ");
+        in=personas.size();
+        do{
+            for (Persona persona : personas) {
+                nom=v.solicitarString("Nombre:");
+                do{
+                    if(ban==0)a=1;
+                    else a=v.solicitarEntero("¿Es aportante?(1: si/0: no)");
+                }while(a<0||a>1);
+                if(a==1){
+                    persona.setContribuye(false);
+                    b=persona.getContribuye();
+                    personas.add(new Persona(b, nom));
+                }else{
+                    b=persona.getContribuye();
+                    slr=v.solicitarDouble("Ingrese el salario del contribuyente");
+                    prc=v.solicitarEntero("Ingrese el porcentaje del salario a aportar");
+                    prc=prc/100;
+                    cnt=slr-slr*prc;
+                    cont.add(new Contribuyentes(b,nom,cnt));
+                }
+                ToAporte+=cnt;
+            }
+        }while(in<=20&&in>1);
     }
-    void ListGastos(){
-    ArrayList<Gasto> gast=new ArrayList<Gasto>();
+    
+    public void aportante(int a){
+        
+    }
+
+    public void PedirGastos(){
        String des;
        double gas;
        boolean b;
-       int i=0;
-       char a = 0;
-       while(a!='N'||a!='n'){
-           des=v.solicitarString("Ingrese descripcion del gasto:");
-           gas=v.solicitarDouble("Ingrese valor del gasto:");
-           
-           do{
-           a=v.pedirChar("Quiere Agregar otro gasto:");
-           b=validarChar(a);
-           if(!b)
-             v.mostrarRes("ingrese una opcion valida");
-       }while(!b);
-          gast.add(new Gasto(des,gas));
-           i++;
-           ToGastos+=gas;
-       }
-       
-   }
+       int i=0, a;
+
+       do{
+            des=v.solicitarString("Ingrese descripcion del gasto:");
+            gas=v.solicitarDouble("Ingrese valor del gasto:");
+            gast.add(new Gasto(gas, des));
+            ToGastos+=gas;
+            a=v.solicitarEntero("¿Desea agregar otro gasto? (1: si/0: no)");
+       }while(a==1);
+    }
+
+
 
     void mostrarLInt(){
-    
-     for( int i=0;i<personas.length;i++)
-     v.mostrarRes(personas.get(i));
+        String m="";
+        for(Persona per:personas){
+            if(!per.getContribuye())
+                m+="Nombre: "+per.getNombre()+".";
+
+        }
+        v.mostrarRes(m);
     }
     void mostrarLCont(){
-    
-     for( int i=0;i<cont.length;i++)
-     v.mostrarRes(cont.get(i));
+        String m="";
+        for(Contribuyentes con:cont){
+            if(con.getContribuye())
+                m+="Nombre: "+con.getNombre()+" Contribucion: "+con.getContribucion();
+        }
+        v.mostrarRes(m);
     }
- 
+
     void mostrarLGast(){
-    
-     for( int i=0;i<gast.length;i++)
-     v.mostrarRes(gast.get(i));
+        String m="";
+        for(Gasto gst:gast){
+            m+=gst.getDescrip()+" $"+gst.getCosto();
+        }
+        v.mostrarRes(m);
+    }
+
+    public void crearArchivo(int c){
+        try{
+            if(c==1) gastos=new File("../gastos.txt");
+                else ahorros=new File("../ahorros.txt");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
     
     public void escribirArchivo(double valor, File arch){
@@ -173,64 +112,61 @@ public class Casa {
             e.printStackTrace();
         }
     }
-    
+
     public String leerArchivo(File f){
-        
+ String linea=null;
         try{
             FileReader fr=new FileReader(f);
             BufferedReader br=new BufferedReader(fr);
-             String linea=null;
+            
              while((linea=br.readLine())!=null){
                  System.out.println (linea + "\n");
-                 return linea;
+                 
            }
           fr.close();
         }catch(Exception e){
            e.printStackTrace();
+           return linea;
         }
+        return linea;
     }
-    
+
     public void compGastos() throws IOException{
         //compGastos(PedirGastos());
         String b=leerArchivo(gastos);
         if(b==null){
             escribirArchivo(0,gastos);
         }else{
-           escribirArchivo(gt,gastos);
-           
+           escribirArchivo(ToGastos,gastos);
+
            double GastosPas=Double.parseDouble(b);
            double aux=GastosPas;
-           aux=+gt;
+           aux=+ToGastos;
            v.mostrarRes("Los gastos de los meses pasados fueron de un total acumulado de "
-                            +GastosPas+"pesos, y los de este mes fueron de "+gt+
+                            +GastosPas+"pesos, y los de este mes fueron de "+ToGastos+
                             " pesos. Todo esto"+ "nos da un total de "+aux+" pesos.");
-           //gastos.delete();
-           BufferedWriter bw = new BufferedWriter(new FileWriter(gastos));
-           String l="";
-           bw.write(l);
-           bw.close();
+           gastos.delete();
+           crearArchivo(1);
            escribirArchivo(aux,gastos);
         }
         }
-    
-    
-    public void calcularAhorro(double gastoMensual,double ingresoMensual){
-        double restante=ingresoMensual-gastoMensual;
+
+
+    public void calcularAhorro(){
+        double restante=ToAporte-ToGastos;
         if(restante<0){
-            v.mostrarRes("""
-                         Sus deudas/gastos son mayores a la cantidad de ingresos que tiene.
-                          \nPor favor intente ingresar nuevamente la cantidad de diero que aporta cada uno.""");
-            pideAportantes();//Cristo no la ha creado
+            v.mostrarRes("Sus deudas/gastos son mayores a la cantidad de ingresos que tiene.");
+           
         }else{
-            
+
             do{
                 v.mostrarRes("El restante de sus ingresos es de "+restante+" pesos");
             ahorro=v.solicitarDouble("¿Cuanto de esto que le sobro desea depositar a su ahorro?");
             validarAhorro(restante,ahorro);
             }while(!validarAhorro(restante,ahorro));
-            
+
         }
-                
+
     }
     boolean validarAhorro(double restante, double ahorro){
         boolean ban=true;
@@ -240,9 +176,9 @@ public class Casa {
         }
         return ban;
     }
-    
+
     public void compAhorros() throws IOException{
-        //compGastos(PedirGastos());
+        //compGastos(PedirGastos());    
         String b=leerArchivo(ahorros);
         if(b==null){
             escribirArchivo(0,ahorros);
@@ -254,18 +190,15 @@ public class Casa {
            v.mostrarRes("Los ahorros de los meses pasados fueron de un total acumulado de "
                             +ahorrosPas+"pesos, y los de este mes fueron de "+ahorro+
                             " pesos. Todo esto"+ "nos da un total de "+aux+" pesos.");
-           //gastos.delete();
-           BufferedWriter bw = new BufferedWriter(new FileWriter(ahorros));
-           String l="";
-           bw.write(l);
-           bw.close();
+           ahorros.delete();
+           crearArchivo(2);
            escribirArchivo(aux,ahorros);
         }
         }
-    
+
     public void menu() throws IOException{
-        //pedirIntegrantes();
-        //PedirGastos();
+        pedirIntegrantes(1);
+        PedirGastos();
         int op;
         do{
             op=v.solicitarEntero("Bienvenido\n ¿Cual de las siguientes acciones desea realizar?"
@@ -273,16 +206,22 @@ public class Casa {
                 + "\n2. Agregar aportantantes."
                 + "\n3. Comparar los gastos actuales con los pasados."
                 + "\n4. Comparar los ahorros actuaes con los pasados"
-                + "\n5. Agregar gasto."
-                + "\n6. Salir"); 
+                + "\n5. Ahorrar dinero."
+                + "\n6. Agregar gasto"
+                + "\n7. Salir"); 
             switch(op){
-            case 1:mostrar();break;
-            case 2: ;break;
+            case 1:
+                mostrarLInt();
+                mostrarLCont();
+                mostrarLGast();break;
+            case 2://0 para ingresar aportante
+                pedirIntegrantes(0) ;break;
             case 3:compGastos();break;
             case 4:compAhorros();break;
-            case 5: ;break;
-            case 6:;break;
+            case 5:calcularAhorro();break;
+            case 6:PedirGastos();break;
+            case 7:;break;
             default:;break;
         }
-        }while(op!=6);}
+        }while(op!=7);}
     }
