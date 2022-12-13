ee16cf68f7ee7d36ba8bb5ee962259870fbd0aa4
MagnusAleksandar <90014085+MagnusAleksandar@users.noreply.github.com>
2022-12-13 00:14:30 -0500

    Menu actualizado AGAIN ¬¬

--- EconApp/src/main/java/Control/Ejecutar.java
+++ EconApp/src/main/java/Control/Ejecutar.java
@@ -6,9 +6,9 @@ public class Ejecutar {
     static Casa ob1=new Casa();
     static InOut ob2=new InOut();
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int op;
-        menu(op);
+        ob1.menu();
     }
     
 }
